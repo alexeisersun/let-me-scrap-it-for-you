a84a3c60f515432e84e5d52a92487d7c3e837602
Alexei Șerșun <alex.sersun@yahoo.com>
2019-06-11 15:15:13 +0300

    Print account details.

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -10,6 +10,7 @@ class Crawler
   def run
     open
     pass_login
+    print_accounts
     close
   end
 
@@ -22,11 +23,33 @@ class Crawler
 
   def pass_login
     demo_link = @browser.link(css: "#demo-link").wait_until(&:present?)
-    p demo_link
     demo_link.click
   end
 
   def close
     @browser.close
+  end
+
+  def print_accounts
+    account_details = []
+    accounts_table = @browser.table(id: "dashboardAccounts").wait_until(&:present?).tbody
+    accounts_table.rows.each do |r|
+      r.wait_until(&:present?).link.click
+      account_info = @browser.div(css: '.acc-info').wait_until(&:present?)
+      fields = [:branch, :currency, :owner, :role, :category]
+      info = account_info.dl.to_hash
+
+      balance = @browser.div(css: '.acc-bal-directive').h3s[0].text
+
+      @browser.back
+      @browser.table(id: "dashboardAccounts").wait_until(&:present?)
+
+      account_details <<
+      {
+        name: info['Титуляр:'], currency: info['Валута:'], balance: balance,
+        nature: info['Вид:']
+      }
+    end
+    puts account_details
   end
 end
