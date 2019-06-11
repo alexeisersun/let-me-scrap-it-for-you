ce40f31a75491e1d6563b8cf66c237d73f14830e
Alexei Șerșun <alex.sersun@yahoo.com>
2019-06-11 11:22:37 +0300

    Create dummy crawler.

--- app.rb
+++ app.rb
@@ -1,2 +1,7 @@
 at_exit { puts "I crawled it for you. <3" }
 puts "Let me crawl it for you."
+
+require_relative 'lib/crawler'
+
+crawler = Crawler.new(browser: :firefox)
+crawler.run
---
+++ lib/crawler.rb
@@ -0,0 +1,10 @@
+require 'watir'
+
+class Crawler
+  def initialize(browser: nil)
+    @browser = browser
+  end
+
+  def run
+  end
+end
