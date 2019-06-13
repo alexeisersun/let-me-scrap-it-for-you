87c9d2ece7cddf11bd2da538f9c6ce50d55350c4
Alexei Șerșun <alex.sersun@yahoo.com>
2019-06-13 10:27:44 +0300

    Rescue NoMethodError in AccountPage.

--- lib/account_page.rb
+++ lib/account_page.rb
@@ -7,13 +7,23 @@ class AccountPage
   end
 
   def fetch_account_into(name, container)
-    @browser.div(css: '.acc-info').wait_until(&:present?)
+    begin
+      account_info_div
 
-    html = Nokogiri::HTML(@browser.html)
-    account_info = html.css('div.acc-info dl dd')
-    balance, currency = html.at_css('.acc-bal-directive h3').content.split
-    balance = balance.to_f
-    nature = account_info[4].content
-    container << Account.new(name, balance, currency, nature)
+      html = Nokogiri::HTML(@browser.html)
+      account_info = html.css('div.acc-info dl dd')
+      balance, currency = html.at_css('.acc-bal-directive h3').content.split
+      balance = balance.to_f
+      nature = account_info[4].content
+      container << Account.new(name, balance, currency, nature)
+    rescue NoMethodError => e
+      sleep(1)
+      account_info_div
+      retry
+    end
+  end
+
+  def account_info_div
+    @browser.div(css: '.acc-info').wait_until(&:present?)
   end
 end
